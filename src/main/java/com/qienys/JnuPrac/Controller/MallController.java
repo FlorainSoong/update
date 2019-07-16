a5237f324d529d46036cae19c1583353e5bad95f
FlorainSoong <38419035+FlorainSoong@users.noreply.github.com>
2019-07-16 14:53:00 +0800

    Update MallController.java

--- src/main/java/com/qienys/JnuPrac/Controller/MallController.java
+++ src/main/java/com/qienys/JnuPrac/Controller/MallController.java
@@ -10,15 +10,40 @@ import com.qienys.JnuPrac.util.MD5Utils;
 import org.apache.shiro.SecurityUtils;
 import org.apache.shiro.authc.*;
 import org.apache.shiro.subject.Subject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+import static java.lang.System.out;
+
 @Controller
 public class MallController {
 
+    @Autowired
     private ProductServiceImpl productServiceImpl;
+
+    @RequestMapping(value = "/productlist", method = RequestMethod.GET, produces = "application/json;charset=UTF-8")
+    @ResponseBody
+    public String getProductList(@RequestBody JSONObject jsonProduct){
+
+        out.println(jsonProduct.toJSONString());
+        JSONObject result = new JSONObject();
+        Product product = JSON.parseObject(jsonProduct.toJSONString(),Product.class);
+        Product productInfo = productServiceImpl.findByProductName(product.getName());
+        out.println(productInfo.getName());
+        result.put("name", product.getName());
+        result.put("descryption",product.getDescription());
+        result.put("price", product.getPrice());
+        result.put("brand", product.getBrand());
+        result.put("sold",product.getSold());
+        result.put("stock", product.getStock());
+        result.put("product type", product.getProductType());
+
+        return result.toJSONString();
+
+    }
 
 
 }
