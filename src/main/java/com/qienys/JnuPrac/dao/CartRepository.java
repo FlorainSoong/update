e5f5e9e1e39d96776c39f109dacbd6be6826eb61
FlorainSoong <38419035+FlorainSoong@users.noreply.github.com>
2019-07-16 16:50:29 +0800

    Add files via upload

--- src/main/java/com/qienys/JnuPrac/dao/CartRepository.java
+++ src/main/java/com/qienys/JnuPrac/dao/CartRepository.java
@@ -1,9 +1,10 @@
 package com.qienys.JnuPrac.dao;
 
 import com.qienys.JnuPrac.pojo.Cart;
+import com.sun.tools.javac.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface CartRepository extends CrudRepository<Cart,Long> {
-
+    List<Cart> findByUid(Long uid);
 
 }
--- src/main/java/com/qienys/JnuPrac/dao/ProductRepository.java
+++ src/main/java/com/qienys/JnuPrac/dao/ProductRepository.java
@@ -1,9 +1,11 @@
 package com.qienys.JnuPrac.dao;
 
 import com.qienys.JnuPrac.pojo.Product;
+import com.sun.tools.javac.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProductRepository extends CrudRepository<Product,Long> {
 
         Product findByName (String name);
+        List<Product> findAll ();
 }
