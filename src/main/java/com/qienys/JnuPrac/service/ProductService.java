2536edf254efa23db92283e3d54060db980eced2
FlorainSoong <38419035+FlorainSoong@users.noreply.github.com>
2019-07-16 14:55:28 +0800

    Update ProductService.java

--- src/main/java/com/qienys/JnuPrac/service/ProductService.java
+++ src/main/java/com/qienys/JnuPrac/service/ProductService.java
@@ -1,4 +1,20 @@
-package com.qienys.JnuPrac.service;
+package com.qienys.JnuPrac.service.impl;
 
-public interface ProductService {
+import com.qienys.JnuPrac.dao.ProductRepository;
+import com.qienys.JnuPrac.pojo.Product;
+import com.qienys.JnuPrac.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductServiceImpl implements ProductService {
+    @Autowired
+    private ProductRepository productRepository;
+
+    public Product findByProductName(String name){
+        Product product = productRepository.findByProductName(name);
+        return product;
+    }
+
 }
+
