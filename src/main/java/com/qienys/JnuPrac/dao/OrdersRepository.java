58b143b8185f8a53641372f8b86ec3c447b19849
FlorainSoong <38419035+FlorainSoong@users.noreply.github.com>
2019-07-17 10:44:00 +0800

    Add files via upload

--- src/main/java/com/qienys/JnuPrac/dao/OrdersRepository.java
+++ src/main/java/com/qienys/JnuPrac/dao/OrdersRepository.java
@@ -1,8 +1,11 @@
 package com.qienys.JnuPrac.dao;
 
 import com.qienys.JnuPrac.pojo.Orders;
+import com.sun.tools.javac.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OrdersRepository extends CrudRepository<Orders,Long> {
+
+    List<Orders> findByUid(Long uid);
 
 }
